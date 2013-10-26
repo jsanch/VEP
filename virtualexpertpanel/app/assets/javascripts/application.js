dbcf0719d388f906d4c23c73fc97f7891eb69ae4
glasslighttech <ksinghal@thrupoint.com>
2013-10-26 13:42:42 -0400

    committing

--- virtualexpertpanel/app/assets/javascripts/application.js
+++ virtualexpertpanel/app/assets/javascripts/application.js
@@ -13,3 +13,13 @@
 //= require jquery
 //= require jquery_ujs
 //= require_tree .
+
+$( document ).ready(function() {
+  var btns = ['btn-one', 'btn-two'];
+  var input = document.getElementById('btn-input');
+  for(var i = 0; i < btns.length; i++) {
+    document.getElementById(btns[i]).addEventListener('click', function() {
+      input.value = this.value;
+    });
+  }
+});
--- virtualexpertpanel/app/controllers/patients_controller.rb
+++ virtualexpertpanel/app/controllers/patients_controller.rb
@@ -1,9 +1,13 @@
 class PatientsController < ApplicationController
   def index
+    @patients = Patient.all
   end
   def new
+    @patient = Patient.new
   end
   def create
+    @patient = Patient.create(params[:patient])
+    redirect_to root_url
   end
   def edit
   end
